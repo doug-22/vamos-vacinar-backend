0a2dd94e2804631ddfff859ac7b493febbb4575a
Douglas Oliveira <douglas.aguiar410@gmail.com>
2022-04-17 19:39:01 -0300

    Creating put route api

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ const Attendance = require("./controllers/attendance.controller")
 
 routes.get("/", Attendance.index);
 routes.get("/api/agendamento", Attendance.listAppointmentByDay);
-routes.post("/api/cadastro", Attendance.createAttendence);
+routes.post("/api/cadastro", Attendance.createAttendance);
+routes.put("/api/editar/:id", Attendance.editAttendance)
 
 module.exports = routes;
