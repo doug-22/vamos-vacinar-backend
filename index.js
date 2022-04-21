f55113977959d45e71f8c478d1c6d96a96a0b7f4
Douglas Oliveira <douglas.aguiar410@gmail.com>
2022-04-21 10:08:11 -0300

    Setting environment variables

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const routes = require("./src/routes")
 const app = express();
@@ -10,5 +11,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(8080, () => {
-    console.log("Server is running on port 8080: http://localhost:8080/");
+    console.log(`Server is running on port 8080: http://localhost:8080/${process.env.PORT}`);
 });
