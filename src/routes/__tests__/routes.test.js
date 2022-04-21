af1f8a03102ab8b4e104049d41b3a7d918bc5beb
Douglas Oliveira <douglas.aguiar410@gmail.com>
2022-04-21 14:35:49 -0300

    Creating test for service registration route

--- src/routes/__tests__/routes.test.js
+++ src/routes/__tests__/routes.test.js
@@ -2,9 +2,26 @@ const request = require("supertest");
 const app = require("../../../app");
 
 describe("Testing api routes", () => {
-  it("should return database", async () => {
-    const res = await request(app).get("/")
 
-    expect(res.body).toEqual([])
-  })
+  it("should be able return empty database", async () => {
+    const res = await request(app).get("/");
+
+    expect(res.body).toEqual([]);
+  });
+
+  it("should be able create attendance", async () => {
+    const res = await request(app)
+      .post("/api/cadastro")
+      .send({
+        name: "Douglas Aguiar Oliveira",
+        birthDate: "1998-10-31",
+        dateAppointment: "2022-04-22",
+        time: "08:00",
+        vaccinated: false
+      });
+    
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("error");
+    expect(res.body.error).toBeFalsy;
+  });
 })
