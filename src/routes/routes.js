e21459fe1d3be60f8aedf20ed35543e59d319685
Douglas <douglas.aguiar410@gmail.com>
2024-09-30 16:23:49 -0300

    feat: add new routes to thullo app

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const routes = express.Router();
 
 const Attendance = require("../controllers/attendance.controller")
+const Thullo = require("../controllers/thullo.controller")
 
 routes.get("/", Attendance.index);
 routes.get("/api/agendamento", Attendance.listAppointmentByDay);
@@ -10,5 +11,8 @@ routes.post("/api/cadastro", Attendance.createAttendance);
 routes.put("/api/editar/:id", Attendance.editAttendance);
 routes.delete("/api/deletar_agendamentos", Attendance.deleteAttendance);
 routes.post("/getaccesstoken", Attendance.getAccessToken);
+
+routes.post("/api/thullo/login", Thullo.login);
+routes.get("/api/thullo/user", Thullo.user);
 
 module.exports = routes;
