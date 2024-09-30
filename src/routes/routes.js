0851fb65f4e7bb3d9699daa0dbffc65b05bf7a73
Douglas <douglas.aguiar410@gmail.com>
2024-09-30 16:46:16 -0300

    feat: fix deploy 3

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,6 @@ const express = require("express");
 const routes = express.Router();
 
 const Attendance = require("../controllers/attendance.controller")
-const Thullo = require("../controllers/thullo.controller")
 
 routes.get("/", Attendance.index);
 routes.get("/api/agendamento", Attendance.listAppointmentByDay);
@@ -11,8 +10,5 @@ routes.post("/api/cadastro", Attendance.createAttendance);
 routes.put("/api/editar/:id", Attendance.editAttendance);
 routes.delete("/api/deletar_agendamentos", Attendance.deleteAttendance);
 routes.post("/getaccesstoken", Attendance.getAccessToken);
-
-// routes.post("/api/thullo/login", Thullo.login);
-// routes.get("/api/thullo/user", Thullo.user);
 
 module.exports = routes;
